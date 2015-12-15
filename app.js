c66b19cb3df5686309a3e1668ee131c9c84763a6
zhy2002 <hz141@uowmail.edu.au>
2015-12-15 23:24:42 +1100

    Setting up

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 9001;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get("/", function(req, res){
+    res.send("Hello world!");
+});
+
+app.get("/books", function(req, res){
+    res.send("Hello books!");
+});
+
+app.listen(port, function(err){
+    console.log("Running server on port " + port);
+});
+
+
+
