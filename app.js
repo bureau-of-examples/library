a21567db405fe35daefee07b48a382455c8b5dcf
zhy2002 <hz141@uowmail.edu.au>
2015-12-16 08:48:48 +1100

    use ejs view template.

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ app.set("views", './src/views');
 
 //app.set("view engine", 'jade');
 
-var handlebars = require('express-handlebars');
-app.engine(".hbs", handlebars({extname: '.hbs'}));
-app.set("view engine", ".hbs");
+//var handlebars = require('express-handlebars');
+//app.engine(".hbs", handlebars({extname: '.hbs'}));
+//app.set("view engine", ".hbs");
+
+app.set("view engine", 'ejs');
 
 app.get("/", function(req, res){
     res.render("index", {title: "Hello from render", list: ['a', 'b']});
+});
+
+app.get("/home", function(req, res){
+    res.render("home", {title: "Hello from render", list: ['my item 1', 'my item 2']});
 });
 
 app.get("/books", function(req, res){
