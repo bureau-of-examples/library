b69646f2a3b1894ebfe5a2387ed8f2a2bf0b9035
zhy2002 <hz141@uowmail.edu.au>
2015-12-17 20:48:08 +1100

    use mongo db to display data.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,26 +1,40 @@
 "use strict";
 
 var express = require('express');
-
-var books = require('../modules/bookCollection');
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var BookRouter = function(args){
     var bookRouter = express.Router();
 
-    //should extend args
-    args.title = "My Books";
-    args.books = books;
-
     bookRouter.route("/")
         .get(function(req, res){
-            res.render("books", args);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db){
+                var collection = db.collection('books');
+                collection.find({}).toArray(function(err, books){
+                    //should extend args
+                    args.title = "My Books";
+                    args.books = books;
+                    res.render("books", args);
+                    db.close();
+                });
+            });
         });
 
     bookRouter.route("/:id")
         .get(function(req, res){
-            var id = req.params.id;
-            args.book = books[id];
-            res.render("book", args);
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db){
+                var collection = db.collection('books');
+                collection.findOne({_id: id}, function(err, book){
+                    //should extend args
+                    args.book = book;
+                    res.render("book", args);
+                    db.close();
+                });
+            });
         });
 
     return bookRouter;
